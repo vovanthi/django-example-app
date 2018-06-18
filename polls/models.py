fb143672bda2d2ccf63fe545d94a277fc2c321ed
vovanthi <vovanthi@sint.co.jp>
2018-06-18 20:30:32 +0900

    add gitignore

--- polls/models.py
+++ polls/models.py
@@ -11,16 +11,15 @@ class Tags(models.Model):
         db_table = 'tags'
     tag_id = models.IntegerField('タグID', primary_key=True)
     tag_name = models.TextField('タグ名', max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 class Categories(models.Model):
     class Meta:
         db_table = 'categories'
-        unique_together = (('tag_id', 'category_id'),)
     tag_id = models.ForeignKey(
         'Tags',
         on_delete=models.DO_NOTHING,
         db_column='tag_id')
-    # tag_id = models.IntegerField('タグID', primary_key=True)
-    category_id = models.IntegerField('カテゴリID', primary_key=True)
     category_name = models.TextField('カテゴリ名', max_length=200)
 
